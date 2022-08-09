665c866736d64d26275d3e3fe5502f728c461e79
SergeyBukhtiyarov <bukhtiyarov.s.a@gmail.com>
2022-08-09 16:10:50 +0400

    add @RequestParam

--- src/main/java/com/example/citydistancecalc/controller/CrowflightCalcController.java
+++ src/main/java/com/example/citydistancecalc/controller/CrowflightCalcController.java
@@ -3,6 +3,7 @@ package com.example.citydistancecalc.controller;
 
 import com.example.citydistancecalc.Crowflight;
 import com.example.citydistancecalc.entity.City;
+import com.example.citydistancecalc.entity.Distance;
 import com.example.citydistancecalc.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,12 +22,16 @@ public class CrowflightCalcController {
 
     @GetMapping()
     public int calculate(@RequestParam(name = "city0") String city0_name,
-                         @RequestParam(name = "city1") String city1_name) {
-      City city0 = cityRepository.findByName(city0_name);
-      City city1 = cityRepository.findByName(city1_name);
-      return crowflight.getDistanceHav(city0,city1);
-
-
+                         @RequestParam(name = "city1") String city1_name,
+                         @RequestParam(name = "calculate") String calculateType) {
+        City city0 = cityRepository.findByName(city0_name);
+        City city1 = cityRepository.findByName(city1_name);
+        if (calculateType.equals("crowflight"))
+            return crowflight.getDistanceHav(city0, city1);
+        else if (calculateType.equals("matrix"))
+            return 0;
+        else return -1;
+    }
 
     }
-}
+
---
+++ src/main/java/com/example/citydistancecalc/repository/DistanceRepository.java
@@ -0,0 +1,4 @@
+package com.example.citydistancecalc.repository;
+
+public interface DistanceRepository {
+}
