57669fdeb231cff45dcda63d39899fca2f0661c7
SergeyBukhtiyarov <bukhtiyarov.s.a@gmail.com>
2022-08-18 10:29:41 +0400

    add List<cities> to class UploadsXMLController add Marshall/Unmarshall to Test

--- src/main/java/com/example/citydistancecalc/controller/UploadsXMLController.java
+++ src/main/java/com/example/citydistancecalc/controller/UploadsXMLController.java
@@ -2,14 +2,17 @@ package com.example.citydistancecalc.controller;
 
 
 import com.example.citydistancecalc.Unmarshaller.UnmarshallXML;
+import com.example.citydistancecalc.entity.Cities;
 import com.example.citydistancecalc.entity.City;
 import com.example.citydistancecalc.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.bind.JAXBException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/uploads")
@@ -28,11 +31,15 @@ public class UploadsXMLController {
 
 
 @PostMapping
-    public ResponseEntity postInfo (@RequestBody City city){
-        cityRepository.save(city);
-    System.out.println(city);
+    public ResponseEntity postInfo (@RequestBody Cities cities){
+        cityRepository.saveAll(cities.getCities());
+    System.out.println(cities);
     return new ResponseEntity(HttpStatus.OK);
 }
-
+//    @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE,produces = MediaType.APPLICATION_XML_VALUE)
+//    public ResponseEntity postInfo (@RequestBody City city){
+//       System.out.println(city);
+//        return new ResponseEntity(HttpStatus.OK);
+//    }
 }
 
